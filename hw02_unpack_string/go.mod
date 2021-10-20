c3fea370003d053e97b2bc24368c0bd1b6ff44ca
Manikainen Aleksandr <manikainen@selectel.ru>
2021-10-20 03:16:08 +0300

    HW2 is completed

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/manegspb/hw-golang-otus/hw02_unpack_string
 
 go 1.16
 
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,11 +16,13 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `пр3ув`, expected: `прррув`},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "DeK2e4", expected: "DeKKeeee"},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +36,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", " f3e", "f3e rea", "f3e\\ rea"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
